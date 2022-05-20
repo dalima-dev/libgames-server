a0f7dd065880425a13608ae30b6b177b04bfdd07
Daniel Alves <dalima.dev@gmail.com>
2022-05-20 11:51:33 -0300

    feat(game dto): implementing validators

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,6 +1,20 @@
+import { IsDateString, IsNumber, IsString, Length, Max, Min } from 'class-validator';
+
 export class createGameDto {
+  @IsString({ message: 'Title is not a string!' })
+  @Length(2, 30)
   title: string;
+
+  @IsString({ message: 'Description is not a string!' })
+  @Length(10, 200)
   description: string;
-  year: number;
+
+  @IsString({ message: 'Year is not a string!' })
+  @IsDateString()
+  year: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   imdbScore: number;
 }
