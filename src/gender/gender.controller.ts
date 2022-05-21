c14d6907bbdd574b9e6c436bff8833bb6abb957a
Daniel Alves <dalima.dev@gmail.com>
2022-05-21 11:00:52 -0300

    feat: implement swagger on gender controller

--- src/gender/gender.controller.ts
+++ src/gender/gender.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { GenderService } from './gender.service';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('gender')
 @Controller('gender')
 export class GenderController {
   constructor(private readonly genderService: GenderService) {}
 
   @Post()
+  @ApiOperation({ summary: 'This adds a new gender.' })
   create(@Body() createGenderDto: CreateGenderDto) {
     return this.genderService.create(createGenderDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'This returns all genders.' })
   findAll() {
     return this.genderService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'This returns a gender by id.' })
   findOne(@Param('id') id: string) {
     return this.genderService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'This updates a gender by id.' })
   update(@Param('id') id: string, @Body() updateGenderDto: UpdateGenderDto) {
     return this.genderService.update(+id, updateGenderDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'This remove a gender by id.' })
   remove(@Param('id') id: string) {
     return this.genderService.remove(+id);
   }
