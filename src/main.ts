dd8b7a19fcebc683b609ec32ed5e9608db36ed07
Daniel Alves <dalima.dev@gmail.com>
2022-05-21 02:16:53 -0300

    fix main swagger

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Game Store API Routes')
+    .setDescription('This is our game store API.')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
