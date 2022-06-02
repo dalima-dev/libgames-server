1533bcc10d93dab4fee823f9761a089d68a41c9c
Daniel Alves <dalima.dev@gmail.com>
2022-06-02 18:18:12 -0300

    feat: setting default strategy to jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
