451fbb7252dd9f6cebdd838090453d561a61b648
Daniel Alves <dalima.dev@gmail.com>
2022-06-02 20:20:56 -0300

    feat(auth): implements 'JwtModule' & 'JwtStrategy class' in module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
     PassportModule.register({
       defaultStrategy: 'jwt',
     }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
