8dafa157f162c00e4ae2041c1884dc6f806a5e96
daniel <dalima.dev@gmail.com>
2022-06-06 02:49:51 -0300

    feat(game): implementing 'adminError'

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -12,6 +12,9 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { adminError } from 'src/utils/admin-error.util';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { GameService } from './game.service';
@@ -37,20 +40,27 @@ export class GameController {
 
   @Post()
   @ApiOperation({ summary: 'Create new game.' })
-  create(@Body() dto: CreateGameDto) {
+  create(@LoggedUser() user: User, @Body() dto: CreateGameDto) {
+    adminError(user);
     return this.gameService.create(dto);
   }
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update new game by ID.' })
-  update(@Param('id') id: string, @Body() dto: UpdateGameDto) {
+  update(
+    @LoggedUser() user: User,
+    @Param('id') id: string,
+    @Body() dto: UpdateGameDto,
+  ) {
+    adminError(user);
     return this.gameService.update(id, dto);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a game by ID.' })
-  delete(@Param('id') id: string) {
+  delete(@LoggedUser() user: User, @Param('id') id: string) {
+    adminError(user);
     return this.gameService.delete(id);
   }
 }
